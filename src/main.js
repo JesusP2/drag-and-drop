f35ab8a1434088ba93cdf52c3624cb8c448dbf2f
JesusP2 <jesus_perez99@hotmail.com>
2025-04-29 00:10:15 -0600

    improvements

--- src/main.js
+++ src/main.js
@@ -1,28 +1,81 @@
 import "./style.css";
 
-const draggable = document.querySelector(".draggable");
+const draggableElement = document.querySelector(".draggable");
+const dropZones = document.querySelectorAll(".dropzone");
 const handleMouseDown = (e) => {
-  const startX = e.clientX;
-  const startY = e.clientY;
-  const { left, top } = draggable.getBoundingClientRect();
-  const position = draggable.style.position;
-  draggable.style.position = "absolute";
-  draggable.style.zIndex = 1000;
+  const mouseStartPosition = { x: e.clientX, y: e.clientY };
+  // get initial position of draggable
+  const { left: elementStartX, top: elementStartY } = draggableElement.getBoundingClientRect();
+  const originalStyles = {
+    position: draggableElement.style.position,
+    zIndex: draggableElement.style.zIndex,
+    width: draggableElement.style.width,
+    height: draggableElement.style.height,
+  };
+  // change to absolute to make it draggable and z-index to move it to the top
+  draggableElement.style.position = "absolute";
+  draggableElement.style.zIndex = 1000;
+  let activeDropZone = null;
 
   const handleMouseMove = (e) => {
-    const deltaX = e.clientX - startX;
-    const deltaY = e.clientY - startY;
-    draggable.style.left = `${deltaX + left}px`;
-    draggable.style.top = `${deltaY + top}px`;
+    // calculate how much the mouse has moved and add it
+    // to the draggable's initial position
+    const deltaX = e.clientX - mouseStartPosition.x;
+    const deltaY = e.clientY - mouseStartPosition.y;
+    draggableElement.style.left = `${elementStartX + deltaX}px`;
+    draggableElement.style.top = `${elementStartY + deltaY}px`;
+
+    // calculate the overlap area between the draggable and the dropzones
+    const draggableRect = draggableElement.getBoundingClientRect();
+    const draggableArea = draggableRect.width * draggableRect.height;
+    dropZones.forEach((dropZone) => {
+      const dropZoneRect = dropZone.getBoundingClientRect();
+      const overlapArea = getOverlapArea(draggableRect, dropZoneRect);
+
+      if (overlapArea > draggableArea * 0.5) {
+        draggableElement.style.height = `${dropZoneRect.height}px`;
+        draggableElement.style.width = `${dropZoneRect.width}px`;
+        activeDropZone = dropZone;
+      } else {
+        draggableElement.style.backgroundColor = "";
+      }
+    });
   };
 
   const handleMouseUp = () => {
     document.removeEventListener("mousemove", handleMouseMove);
     document.removeEventListener("mouseup", handleMouseUp);
-    draggable.style.position = position;
+    // reset the position and z-index
+    draggableElement.style.position = originalStyles.position;
+    draggableElement.style.zIndex = originalStyles.zIndex;
+    // if dropZone is found, append the draggable to the dropZone,
+    // otherwise, reset the position
+    if (activeDropZone) {
+      const { left, top } = activeDropZone.getBoundingClientRect();
+      draggableElement.style.left = `${left}px`;
+      draggableElement.style.top = `${top}px`;
+      activeDropZone.appendChild(draggableElement);
+    } else {
+      draggableElement.style.left = `${elementStartX}px`;
+      draggableElement.style.top = `${elementStartY}px`;
+      draggableElement.style.width = originalStyles.width;
+      draggableElement.style.height = originalStyles.height;
+    }
   };
 
   document.addEventListener("mousemove", handleMouseMove);
   document.addEventListener("mouseup", handleMouseUp);
 };
-draggable.addEventListener("mousedown", handleMouseDown);
+draggableElement.addEventListener("mousedown", handleMouseDown);
+
+function getOverlapArea(rect1, rect2) {
+  const xOverlap = Math.max(
+    0,
+    Math.min(rect1.right, rect2.right) - Math.max(rect1.left, rect2.left),
+  );
+  const yOverlap = Math.max(
+    0,
+    Math.min(rect1.bottom, rect2.bottom) - Math.max(rect1.top, rect2.top),
+  );
+  return xOverlap * yOverlap;
+}
